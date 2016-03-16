95966e86c421bf46a56776585041dae342e8be7b
Surya Gullapalli <surya.gullapalli@ddu-india.com>
2016-03-16 15:18:29 +0530

    Alembic WIP

--- tools/alembic.py
+++ tools/alembic.py
@@ -1,5 +1,10 @@
 from waflib.Configure import conf
 
+def options(ctx):
+    pass
+
 def configure(ctx):
     ctx.start_msg('Checking for alembic')
-    ctx.end_msg('Done')
+    lib_names = ['Abc', 'AbcCollection', 'AbcCoreAbstract', 'AbcCoreFactory',
+                 'AbcCoreHDF5', 'AbcCoreOgawa', 'AbcGeom', 'AbcMaterial',
+                 'AbcOpenGL', 'Ogawa', 'Util', 'WFObjConvert']
